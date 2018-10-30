3afba36debe1b3cf863543b5834ad2e43a5099de
roylevy1 <38298930+roylevy1@users.noreply.github.com>
2018-10-30 13:08:34 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -3,10 +3,19 @@ from bs4 import BeautifulSoup
 import time
 import datetime
 
-MAX_ELECTORS = 182983
+SLEEP_TIME = 60
+CITY_NAME = "אשדוד"
+
 links = {
-    "votes": "https://services7.arcgis.com/gvSC24scGQBSxl3v/arcgis/rest/services/retzef_JS_election_WGS_1984_Atar_tsiburi/FeatureServer/0/query?f=json&where=(UPPER(muni_heb)%20%3D%20UPPER(%27%D7%90%D7%A9%D7%93%D7%95%D7%93%27))&returnGeometry=false&spatialRel=esriSpatialRelIntersects&geometry=%7B%22xmin%22%3A3848510.131586918%2C%22ymin%22%3A3731694.212856393%2C%22xmax%22%3A3868135.3385991105%2C%22ymax%22%3A3744516.5243512136%2C%22spatialReference%22%3A%7B%22wkid%22%3A102100%2C%22latestWkid%22%3A3857%7D%7D&geometryType=esriGeometryEnvelope&inSR=102100&outStatistics=%5B%7B%22statisticType%22%3A%22sum%22%2C%22onStatisticField%22%3A%22VotersInRa%22%2C%22outStatisticFieldName%22%3A%22VOTERSINRA_SUM%22%7D%5D&outSR=102100",
-    "avg": "https://services7.arcgis.com/gvSC24scGQBSxl3v/arcgis/rest/services/retzef_JS_election_WGS_1984_Atar_tsiburi/FeatureServer/0/query?f=json&where=(UPPER(muni_heb)%20%3D%20UPPER(%27%D7%90%D7%A9%D7%93%D7%95%D7%93%27))&returnGeometry=false&spatialRel=esriSpatialRelIntersects&geometry=%7B%22xmin%22%3A3847564.22336189%2C%22ymin%22%3A3720964.3650108753%2C%22xmax%22%3A3888305.1594378343%2C%22ymax%22%3A3746608.9880005177%2C%22spatialReference%22%3A%7B%22wkid%22%3A102100%7D%7D&geometryType=esriGeometryEnvelope&inSR=102100&outStatistics=%5B%7B%22statisticType%22%3A%22avg%22%2C%22onStatisticField%22%3A%22vs2013%22%2C%22outStatisticFieldName%22%3A%22VS2013_AVG%22%7D%5D&outSR=102100"}
+    "electors": "https://services7.arcgis.com/gvSC24scGQBSxl3v/arcgis/rest/services/retzef_JS_election_WGS_1984_Atar_tsiburi/FeatureServer/0/query?f=json&where=(UPPER(muni_heb)%20=%20UPPER('" + CITY_NAME + "'))&outStatistics=[{%22statisticType%22%3A%22sum%22%2C%22onStatisticField%22%3A%22Registered%22%2C%22outStatisticFieldName%22%3A%22REGISTERED_SUM%22}]&outSR=102100",
+    "votes": "https://services7.arcgis.com/gvSC24scGQBSxl3v/arcgis/rest/services/retzef_JS_election_WGS_1984_Atar_tsiburi/FeatureServer/0/query?f=json&where=(UPPER(muni_heb)%20=%20UPPER('" + CITY_NAME + "'))&outStatistics=[{%22statisticType%22:%22sum%22,%22onStatisticField%22:%22VotersInRa%22,%22outStatisticFieldName%22:%22VOTERSINRA_SUM%22}]&outSR=102100",
+    "avg": "https://services7.arcgis.com/gvSC24scGQBSxl3v/arcgis/rest/services/retzef_JS_election_WGS_1984_Atar_tsiburi/FeatureServer/0/query?f=json&where=(UPPER(muni_heb)%20=%20UPPER('" + CITY_NAME + "'))&outStatistics=%5B%7B%22statisticType%22%3A%22avg%22%2C%22onStatisticField%22%3A%22vs2013%22%2C%22outStatisticFieldName%22%3A%22VS2013_AVG%22%7D%5D&outSR=102100"}
+
+page = requests.get(links["electors"])
+soup = BeautifulSoup(page.text, 'html.parser')
+electors = str(soup).split('"REGISTERED_SUM":')[1].split("}")[0]
+max_electors = int(electors)
+
 while (datetime.datetime.now().hour < 22):
     page = requests.get(links["votes"])
     soup = BeautifulSoup(page.text, 'html.parser')
@@ -18,7 +27,7 @@ while (datetime.datetime.now().hour < 22):
     old = str(soup).split('"VS2013_AVG":')[1].split("}")[0]
     old = str(float(old) * 100) + "%"
 
-    current_avg = vote / MAX_ELECTORS * 100
+    current_avg = vote / max_electors * 100
 
     file = open("stats.txt", "a")
     file.write(str(vote) + "\n")
@@ -28,6 +37,6 @@ while (datetime.datetime.now().hour < 22):
     file.write("\n")
     file.close()
 
-    time.sleep(60)
+    time.sleep(SLEEP_TIME)
 
 print("happy end election")
